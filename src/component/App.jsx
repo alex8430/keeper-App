4da8d32780b635f55afba066a16269c4ac44a576
alex8430 <pv24aug2001@gmail.com>
2020-06-19 13:31:05 +0530

    add property to render notes dynamicaly

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Name from "./Header";
 import Copyright from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App(){
-    return (<div><Name />  <Note /><Copyright /></div>);
+    return (<div><Name /> {notes.map(x => (<Note key ={x.key} title = {x.title} content = {x.content} />))} <Copyright /></div>);
 }
+
 
 export default App;
--- src/component/Note.jsx
+++ src/component/Note.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-function Note(){
+
+function Note(props){
     return (<div className = "note">
-        <h1>Title</h1>
-        <p>Your Content is Nothing!</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>);
 }
+
+
 
 export default Note;
